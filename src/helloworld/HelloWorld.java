27f10b1180b0c2186ecde80ff893280111e7e95f
bwstevens <bwstevens@WS205-013.ad.coloradomesa.edu>
2016-08-30 12:09:58 -0600

    basic hello commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+import static kiss.API.*;
+
+/**
+ *
+ * @author bwstevens
+ */
+public class HelloWorld {
+    void untestRun(){
+        outExpect("Hello World!");
+        run();
+        outClose();
+    }
+    
+    void run() {
+        println("Hello World!");
+    }
+    }
+
+       
+    
+
