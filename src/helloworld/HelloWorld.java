cf43c0ab1c9718f7f0ef24f5a2fc915701ca6310
bwstevens <bwstevens@WS205-013.ad.coloradomesa.edu>
2016-09-29 13:36:21 -0600

    collections

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -5,6 +5,8 @@
  */
 package helloworld;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import static kiss.API.*;
 
 /**
@@ -320,5 +322,50 @@ public class HelloWorld {
             TimeZoneClock tzClock = testAlways(new TimeZoneClock());
         }
         
+        void testBoxing(){
+            Integer x = new Integer(3);
+            Integer y = x;
+            ++x;
+            //x = new Integer(x.intValue()+1);
+            assert x == 4;
+            assert y == 3;
+            assert y != new Integer(3);
+            assert y.equals(new Integer(3));
+            assert y.compareTo(new Integer(3)) == 0;
+        }
         
+        //inner class
+       public class Reverse implements Comparator<Integer>{
+        @Override
+        public int compare(Integer a, Integer b){
+        return -a.compareTo(b);
+        }
+       }
+        
+        void testArrayListInt(){
+            ArrayList<Integer> ages = new ArrayList<Integer>();
+            ages.add(3);
+            ages.add(17);
+            ages.add(100);
+            
+            for(int age : ages){
+                println(age);
+            }
+            
+            ages.sort(new IntegerReverse()); //just a class
+            ages.sort(new Reverse()); // inner class
+            ages.sort(new Comparator<Integer>() { //anonymous inner class
+                 @Override
+                 public int compare(Integer a, Integer b){
+                    return -a.compareTo(b);
+                 }
+            });
+            
+            ages.sort((a,b) -> -a.compareTo(b)); //lambda (anonymous funciton)
+               for(int age : ages){
+                println(age);
+            }
+        }
+        
+            
     }
